24c6486523e6c265b7bd38f7de6c81a6fea7afa0
Andrea Giammarchi <agiammarchi@twitter.com>
2014-09-25 23:49:59 +0100

    libary and tests

--- README.md
+++ README.md
@@ -3,3 +3,36 @@ lazyval
 
 [![build status](https://secure.travis-ci.org/WebReflection/lazyval.png)](http://travis-ci.org/WebReflection/lazyval)
 
+A simple function to define lazy properties to a generic object or a prototype.
+
+There's nothing special on this function except it solves problem with older Android phones and IE9 Mobile and it's compatible with mostly every browser or engine out there.
+
+### examples via inheritance
+
+```js
+function A() {}
+lazyval(
+  A.prototype,
+  'expensive',
+  function () {
+    // do expensive computation here
+    // as example ...
+    return Math.random();
+  }
+);
+
+var
+  a1 = new A,
+  a2 = new A
+;
+
+a1.expensive; // 0.346731865
+a1.expensive === a1.expensive; // true
+a2.expensive === a1.expensive; // false
+a2.expensive; // 0.874216789
+
+// clean up
+delete a1.expensive;
+a1.expensive; // 0.4567891
+a1.expensive === a1.expensive; // true
+```
--- test/lazyval.js
+++ test/lazyval.js
@@ -1,13 +1,96 @@
 //remove:
-var main = require('../build/lazyval.node.js');
+var lazyval = require('../build/lazyval.node.js');
 //:remove
 
 wru.test([
   {
-    name: "main",
+    name: 'main',
     test: function () {
-      wru.assert(typeof main == "object");
+      wru.assert(typeof lazyval == 'function');
       // wru.assert(0);
+    }
+  },{
+    name: 'basic object test',
+    test: function () {
+      var o = {};
+      var i = 0;
+      var random;
+      lazyval(o, 'random', function () {
+        i++;
+        return Math.random();
+      });
+      wru.assert('until reached, nothing happens', i === 0);
+      random = o.random;
+      wru.assert('once reached, the value is the expected', /^0\.\d+$/.test(random));
+      wru.assert('reached many times does not affect the value', o.random === o.random);
+      wru.assert('callback invoked only once', i === 1);
+      wru.assert('it is possible to remove the property', delete o.random);
+      wru.assert('now no value assigned', o.random === undefined);
+      wru.assert('callback not invoked again', i === i);
+      wru.assert('we can re-assign the property');
+      lazyval(o, 'random', function () {
+        i++;
+        return Math.random();
+      });
+      wru.assert('[2] until reached, nothing happens', i === 1);
+      wru.assert('[2] once reached, the value is the expected', /^0\.\d+$/.test(o.random));
+      wru.assert('[2] reached many times does not affect the value', o.random === o.random);
+      wru.assert('[2] callback invoked only once', i === 2);
+      wru.assert('[2] new value is different from previous one', random !== o.random);
+      wru.assert('[2] it is possible to remove the property', delete o.random);
+      wru.assert('[2] now no value assigned', o.random === undefined);
+    }
+  }, {
+    name: 'inherited object test',
+    test: function () {
+      function Base() {}
+      lazyval(Base.prototype, 'random', function () {
+        i++;
+        return Math.random();
+      });
+      var o = new Base;
+      var i = 0;
+      var random;
+      wru.assert('until reached, nothing happens', i === 0);
+      random = o.random;
+      wru.assert('once reached, the value is the expected', /^0\.\d+$/.test(random));
+      wru.assert('reached many times does not affect the value', o.random === o.random);
+      wru.assert('callback invoked only once', i === 1);
+      wru.assert('it is possible to remove the property', delete o.random);
+      wru.assert('callback not invoked again', i === i);
+      wru.assert('[2] once reached, the value is the expected', /^0\.\d+$/.test(o.random));
+      wru.assert('[2] reached many times does not affect the value', o.random === o.random);
+      wru.assert('[2] callback invoked only once', i === 2);
+      wru.assert('[2] new value is different from previous one', random !== o.random);
+      wru.assert('[2] it is possible to remove the property', delete o.random);
+    }
+  }, {
+    name: 'inherited and inheritee',
+    test: function () {
+      function Base() {}
+      lazyval(Base.prototype, 'random', function () {
+        return Math.random();
+      });
+      var o = new Base;
+      var random;
+      wru.assert('accessing the proto object affects instances', Base.prototype.random === o.random);
+      wru.assert('the value is the expected',
+        /^0\.\d+$/.test(Base.prototype.random) && /^0\.\d+$/.test(o.random)
+      );
+      random = o.random;
+      lazyval(Base.prototype, 'random', function () {
+        return Math.random();
+      });
+      wru.assert('we can reassign the property', Base.prototype.random !== random);
+      lazyval(Base.prototype, 'random', function () {
+        return Math.random();
+      });
+      wru.assert('once reached via inheritance the value is the expected', /^0\.\d+$/.test(o.random));
+      wru.assert('and it is different from before', random !== o.random);
+      random = Base.prototype.random;
+      delete Base.prototype.random;
+      wru.assert('object has its own property', /^0\.\d+$/.test(o.random));
+      wru.assert('and it is different from the other', random !== o.random);
     }
   }
 ]);
