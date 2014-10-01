d984291080f51f1c19761e839e64d6adbcb6dc58
Andrea Giammarchi <agiammarchi@twitter.com>
2014-10-01 08:24:25 +0100

    made new porperties enumerable

--- build/lazyval.node.js
+++ build/lazyval.node.js
@@ -74,6 +74,8 @@ var lazyval = lazyval || (function (Object) {
             defineProperty(self, property, {
               // making it configurable
               configurable: true,
+              // and making it own property
+              enumerable: true,
               value: value
             });
             // in case it's buggy and the removed descriptor
