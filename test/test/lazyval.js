f9f74f3d82ed75f757b7a806438b4f6155ceef65
Andrea Giammarchi <agiammarchi@twitter.com>
2014-11-02 09:25:53 +0000

    automatic test generator

--- test/test/lazyval.js
+++ test/test/lazyval.js
@@ -7,9 +7,10 @@ wru.test([
     name: 'main',
     test: function () {
       wru.assert(typeof lazyval == 'function');
-      // wru.assert(0);
+      wru.assert(typeof lazyval.direct == 'function');
+      wru.assert(typeof lazyval.proto == 'function');
     }
-  },{
+  }, {
     name: 'basic object test',
     test: function () {
       var o = {};
@@ -20,6 +21,7 @@ wru.test([
         return Math.random();
       });
       wru.assert('until reached, nothing happens', i === 0);
+      wru.assert('reached many times does not affect the value', o.random === o.random);
       random = o.random;
       wru.assert('once reached, the value is the expected', /^0\.\d+$/.test(random));
       wru.assert('reached many times does not affect the value', o.random === o.random);
@@ -91,6 +93,72 @@ wru.test([
       delete Base.prototype.random;
       wru.assert('object has its own property', /^0\.\d+$/.test(o.random));
       wru.assert('and it is different from the other', random !== o.random);
+    }
+  }, {
+    name: 'inherited and inheritee as lazyval.proto',
+    test: function () {
+      function Base() {}
+      lazyval.proto(Base.prototype, 'random', function () {
+        return Math.random();
+      });
+      var o = new Base;
+      var random;
+      var callback;
+      wru.assert('accessing the proto object does not affect instances', Base.prototype.random !== o.random);
+      wru.assert('the value is the expected',
+        typeof Base.prototype.random === 'function' && /^0\.\d+$/.test(o.random)
+      );
+      callback = Base.prototype.random;
+      random = o.random;
+      lazyval(Base.prototype, 'random', function () {
+        return Math.random();
+      });
+      wru.assert('we can reassign the property', Base.prototype.random !== callback);
+      lazyval(Base.prototype, 'random', function () {
+        return Math.random();
+      });
+      delete o.random;
+      wru.assert('once reached via inheritance the value is the expected', /^0\.\d+$/.test(o.random));
+      wru.assert('and it is different from before', random !== o.random);
+      random = Base.prototype.random;
+      delete Base.prototype.random;
+      wru.assert('object has its own property', /^0\.\d+$/.test(o.random));
+      wru.assert('and it is different from the other', random !== o.random);
+    }
+  }, {
+    name: 'properties instead of proto',
+    test: function () {
+      var o = lazyval({}, {
+        a: function () {
+          return 1;
+        },
+        b: function () {
+          return 2;
+        }
+      });
+      wru.assert(3 === o.a + o.b);
+    }
+  }, {
+    name: 'enumerable VS non enumearble property',
+    test: function () {
+      var gOPD = Object.getOwnPropertyDescriptor;
+      var o = {};
+      lazyval(o, 'a', function () { return 1; });
+      lazyval(o, 'b', function () { return 2; }, true);
+      wru.assert('values are OK in both cases', o.a === 1 && o.b === 2);
+      wru.assert('default is not enumerable', !gOPD(o, 'a').enumerable);
+      wru.assert('but it can be enumerable', gOPD(o, 'b').enumerable);
+    }
+  }, {
+    name: 'enumerable VS non enumearble properties',
+    test: function () {
+      var gOPD = Object.getOwnPropertyDescriptor;
+      var o = {};
+      lazyval(o, {a: function () { return 1; }});
+      lazyval(o, {b: function () { return 2; }}, true);
+      wru.assert('values are OK in both cases', o.a === 1 && o.b === 2);
+      wru.assert('default is not enumerable', !gOPD(o, 'a').enumerable);
+      wru.assert('but it can be enumerable', gOPD(o, 'b').enumerable);
     }
   }
 ]);
